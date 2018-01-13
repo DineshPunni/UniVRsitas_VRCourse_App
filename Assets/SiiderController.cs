a4d26f92deeba23548303a8af8e3914d4361a16e
Dinesh <dinesh.punni@hotmail.de>
2018-01-14 00:45:24 +0100

    Ambient music added and its also controlled by the UI Slider now

---
+++ Assets/SiiderController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SiiderController : MonoBehaviour {
+
+    public AudioSource audioSource;
+    private Slider slider;
+
+	void Start () {
+
+        slider = GetComponent<Slider>();
+	}
+	
+	void Update () {
+
+        audioSource.volume = slider.value;
+	}
+}
