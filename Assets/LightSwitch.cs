b329d985070c6406a0ef55cceecc35fd8d78ed6c
Dinesh <dinesh.punni@hotmail.de>
2018-01-14 02:26:20 +0100

    Scene Change on ButtonClick added (Scenechange changes the room you are in) TODO add smooth transition from scene to scene

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightSwitch : MonoBehaviour {
+
+    public Light lightSwitch;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+
+    public void ToggleLight()
+    {
+        lightSwitch.enabled = !lightSwitch.enabled;
+    }
+}
---
+++ Assets/SwitchRoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchRoom : MonoBehaviour {
+
+    public void ChangeScene()
+    {
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+    }
+}
