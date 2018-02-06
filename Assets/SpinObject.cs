65aa61b9ff0568ae393ee577e13a1acb0deb2b4d
Dinesh <dinesh.punni@hotmail.de>
2018-02-06 10:16:43 +0100

    Added object interaction: You need to add a class which derives from VRTK_InteractableObject and add VRKT_Interact Use to the controller

---
+++ Assets/SpinObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SpinObject : VRTK_InteractableObject {
+
+    Transform content;
+    float rotationSpeed = 0;
+
+	void Start () {
+
+        content = transform.GetChild(0);
+	}
+
+    public override void StartUsing(VRTK_InteractUse currentUsingObj)
+    {
+        base.StartUsing(currentUsingObj);
+        rotationSpeed = 200;
+        Debug.Log("startusing");
+    }
+
+
+    public override void StopUsing(VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
+    {
+        base.StopUsing(previousUsingObject, resetUsingObjectState);
+        rotationSpeed = 0;
+
+    }
+
+    protected override void Update ()
+    {
+        content.transform.Rotate(new Vector3(0,rotationSpeed * Time.deltaTime, 0));	
+
+	}
+
+  
+}
